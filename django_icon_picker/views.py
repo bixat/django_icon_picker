e0855e1793cd0d0b182ba4071c526bcd3013d812
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2024-03-23 01:21:22 +0000

    Fixed security issues on download svg endpoint (#4)
    * Handled user permission
    
    * Used parameters instead of pass direct link for avoid security issues
    
    * removed settings.py
    
    * minor fixes
    
    * foramt code

--- django_icon_picker/views.py
+++ django_icon_picker/views.py
@@ -5,27 +5,34 @@ from django.conf import settings
 import requests
 import os
 
-def download_and_save_svg(request, svg_url):
-    id = request.GET.get('id')
-    model = request.GET.get('model')
-    # Define the path where you want to save the SVG file
-    save_path = getattr(settings, 'DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH')
 
-    save_path = save_path + model
-    # Ensure the save path exists
-    os.makedirs(save_path, exist_ok=True)
-    # Download the SVG file
-    response = requests.get(svg_url)
-    if response.status_code == 200:
-        # Extract the filename from the URL
-        filename = os.path.basename(f"icon-{id}.svg")
-        file_path = os.path.join(save_path, filename)
+def download_and_save_svg(request):
+    model = request.GET.get("model")
+    if request.user.is_superuser or request.user.has_perm(f"edit_{model}"):
+        svg_icon = request.GET.get("icon")
+        color = request.GET.get("color").replace("#", "%23")
+        svg_url = f"https://api.iconify.design/{svg_icon}?color={color}"
+        id = request.GET.get("id")
+        # Define the path where you want to save the SVG file
+        save_path = getattr(settings, "DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH")
 
-        # Save the SVG file
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
-        return HttpResponse(file_path)
+        save_path = save_path + model
+        # Ensure the save path exists
+        os.makedirs(save_path, exist_ok=True)
+        # Download the SVG file
+        response = requests.get(svg_url)
+        if response.status_code == 200:
+            # Extract the filename from the URL
+            filename = os.path.basename(f"icon-{id}.svg")
+            file_path = os.path.join(save_path, filename)
+
+            # Save the SVG file
+            with open(file_path, "wb") as f:
+                f.write(response.content)
+            return HttpResponse(file_path)
+        else:
+            return HttpResponse(
+                f"Failed to download SVG file. Status code: {response.status_code}"
+            )
     else:
-        return HttpResponse(f"Failed to download SVG file. Status code: {response.status_code}")
-
-
+        return HttpResponse("Not permitted")

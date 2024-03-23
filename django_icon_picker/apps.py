ec96b853b4be2e795c5b90547038607a1d2fb62c
Mohammed <m97.chahboun@gmail.com>
2024-03-23 01:20:22 +0000

    minor fixes

--- django_icon_picker/apps.py
+++ django_icon_picker/apps.py
@@ -3,11 +3,3 @@ from django.apps import AppConfig
 class DjangoIconPickerConfig(AppConfig):
     name = 'django_icon_picker'
     static_url = '/static/'
-    
-    def ready(self):
-        # Import and load the user's settings
-        from django.conf import settings
-        from .settings import DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH
-
-        # Override default settings with user's settings
-        DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH = getattr(settings, 'DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH', DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH)

1f3c30e6f6da8a77924550951f1d74db16d0ad70
Mohammed <m97.chahboun@gmail.com>
2025-01-16 16:10:43 +0100

    Improved example

--- django_icon_picker_example/example/admin.py
+++ django_icon_picker_example/example/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import ExampleModel
-# Register your models here.
 
-admin.site.register(ExampleModel)
+class ExampleModelAdmin(admin.ModelAdmin):
+    list_display = ['svg_icon', 'name', 'icon']
+
+admin.site.register(ExampleModel, ExampleModelAdmin)

ebe77d06d00ca6a21ccf15802f8f5dc96569ef82
Mohammed CHAHBOUN <M97.chahboun@gmail.com>
2025-01-17 21:09:01 +0100

    bump version

--- django-icon-picker/setup.py
+++ django-icon-picker/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-icon-picker",
-    version="1.0.0",
+    version="1.0.1",
     description="A custom widget for Django forms that allows users to select icons from a predefined set.",
     author="Mohammed CHAHBOUN",
     author_email="m97.chahboun@gmail.com",

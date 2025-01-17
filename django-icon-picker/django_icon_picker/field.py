313667c4872285b6ef2e102b8e03bbabadcd13da
Mohammed CHAHBOUN <M97.chahboun@gmail.com>
2025-01-17 23:41:28 +0100

    Handled remove icon file

--- django-icon-picker/django_icon_picker/field.py
+++ django-icon-picker/django_icon_picker/field.py
@@ -1,7 +1,9 @@
 # fields.py
 from django.db import models
 from .widgets import IconPicker
-
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+import os
 
 class IconField(models.CharField):
     description = "A custom field to store icon information."
@@ -14,3 +16,15 @@ class IconField(models.CharField):
         attrs = {"model_name": self.model.__name__.lower()}
         kwargs["widget"] = IconPicker(attrs=attrs)
         return super().formfield(**kwargs)
+    
+    def contribute_to_class(self, cls, name, **kwargs):
+        super().contribute_to_class(cls, name, **kwargs)
+        pre_delete.connect(self._delete_file, sender=cls)
+
+    def _delete_file(self, sender, instance, **kwargs):
+        file_path = getattr(instance, self.attname)
+        if file_path and os.path.exists(file_path):
+            try:
+                os.remove(file_path)
+            except OSError as e:
+                pass

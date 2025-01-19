17390a05f7cad8dcbf16add0df0c1b266948f7c5
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2025-01-19 12:26:35 +0100

    Handled remove & Update icon file (#17)
    * Handled remove icon file
    
    * Added readme to setup
    
    * removed unused import
    
    * Handle update icon case
    
    * format code using ruff

--- django-icon-picker/django_icon_picker/field.py
+++ django-icon-picker/django_icon_picker/field.py
@@ -1,6 +1,8 @@
 # fields.py
 from django.db import models
 from .widgets import IconPicker
+from django.db.models.signals import pre_delete
+import os
 
 
 class IconField(models.CharField):
@@ -14,3 +16,15 @@ class IconField(models.CharField):
         attrs = {"model_name": self.model.__name__.lower()}
         kwargs["widget"] = IconPicker(attrs=attrs)
         return super().formfield(**kwargs)
+
+    def contribute_to_class(self, cls, name, **kwargs):
+        super().contribute_to_class(cls, name, **kwargs)
+        pre_delete.connect(self._delete_file, sender=cls)
+
+    def _delete_file(self, sender, instance, **kwargs):
+        file_path = getattr(instance, self.attname)
+        if file_path and os.path.exists(file_path):
+            try:
+                os.remove(file_path)
+            except OSError as e:
+                pass
--- django-icon-picker/setup.py
+++ django-icon-picker/setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="django-icon-picker",
     version="1.0.1",
@@ -9,6 +14,8 @@ setup(
     url="https://github.com/bixat/django_icon_picker",
     include_package_data=True,
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         "Django>=3.0",
     ],
